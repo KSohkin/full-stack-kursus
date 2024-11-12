7e61670f2e62821c38a7e5f2e8f2c5b0be58b764
KSohkin <you@example.com>
2024-11-12 14:15:40 +0200

    add greetings refreshed

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ const Hello = (props) => {
 }
 
 const App = () => {
-  const name = 'Peter'
+  const name = 'José Mourinho'
   const age = 10
 
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name='Maya' age={26 + 10} />
+      <Hello name='Gerten' age={26 + 10} />
       <Hello name={name} age={age} />
     </div>
   )
