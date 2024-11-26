7856073a8f1245007f927afc600778c4ad36127c
KSohkin <Kaupo.Sohkin@tptlive.ee>
2024-11-26 15:03:25 +0200

    Voting system TrumpV2.5.124124

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
 import { useState } from "react"
+import './App.css';
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const increasesByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreasesByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-    const setToZero = () => {
-      console.log('resetting to zero, value before', counter)
-      setCounter(0)
-    }
-
-    return (
-      <div>
-        <Display counter = {counter} />
-        <Button onClick={increasesByOne} text="+"/>
-        <Button onClick={setToZero} text="0"/>
-        <Button onClick={decreasesByOne} text="-"/>
-      </div>
-    )
+  return (
+   <div>
+    <div>
+       <h1>Give feedback</h1>
+      <button onClick={() => {setGood(good +1)}}>good</button>
+      <button onClick={() => {setNeutral(neutral +1)}}>neutral</button>
+      <button onClick={() => {setBad(bad +1)}}>bad</button>
+    </div>
+    <h2>statistics</h2>
+    <table>
+      <tr>
+        <td>good</td>
+        <td>{good}</td>
+      </tr>
+    </table>
+    <table>
+      <tr>
+        <td>neutral</td>
+        <td>{neutral}</td>
+      </tr>
+    </table>
+    <table>
+      <tr>
+        <td>bad</td>
+        <td>{bad}</td>
+      </tr>
+    </table>
+    <table>
+      <tr>
+        <td>all</td>
+        <td>{bad + good + neutral}</td>
+      </tr>
+    </table>
+    <table>
+      <tr>
+        <td>average</td>
+        <td>{(good*1 + bad*-1 + neutral*0) / (good + bad + neutral)}</td>
+      </tr>
+    </table>
+    <table>
+      <tr>
+        <td>positive</td>
+        <td>{Math.round(good / (bad + neutral + good) * 100)}%</td>
+      </tr>
+    </table>
+  </div> 
+  )
 }
 
 export default App
